2d6af34d160f89204c6760074d450cb5424f4788
homeworks <go>
2020-08-11 14:53:51 +0400

    go,  homeworks

---
+++ wordcount/wordcount.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This program counts words in the given string")
+	fmt.Println("Enter the string")
+	var input string
+	fmt.Scanf("%s", &input)
+
+	var wordcount int
+	wordcount = 0
+
+
+	for _,char := range input {
+		 if char == ' ' || char == '\n' || char == '\t' {
+			 wordcount++
+		 }
+
+	} 
+
+	wordcount++ // last character
+
+	fmt.Printf("number of words in the string is %d", wordcount)
+
+	
+}
