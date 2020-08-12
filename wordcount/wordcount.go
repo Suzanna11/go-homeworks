212662aa5c45f440f083a33fd4a67151c3cc8ec0
homeworks <go>
2020-08-13 02:53:35 +0400

    go, homeworks

--- wordcount/wordcount.go
+++ wordcount/wordcount.go
@@ -1,25 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
 	fmt.Println("This program counts words in the given string")
 	fmt.Println("Enter the string")
-	var input string
+	
 
-	fmt.Scanf(" %[^\t\n]s", &input)
+	reader := bufio.NewReader(os.Stdin)
+    text, _ := reader.ReadString('\n')
 
 	var wordcount int
 	wordcount = 0
 
-	for _, char := range input {
+	for _, char := range text {
 		if char == ' ' || char == '\n' || char == '\t' {
 			wordcount++
 		}
 
 	}
 
-	wordcount++ // last character
+	
 
 	fmt.Printf("number of words in the string is %d", wordcount)
 
